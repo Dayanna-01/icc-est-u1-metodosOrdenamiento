452d7260ce3210c1857ec2e5a222671435544cd2
Dayanna Chacha <jchachav@est.ups.edu.ec>
2025-10-20 12:48:04 -0500

    Metodo Burbuja

--- src/App.java
+++ src/App.java
@@ -1,13 +1,23 @@
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception { 
         int[] numeros = {10, 0, -5, 5, 15, 2};
 
-        SortBubble sortBubble = new SortBubble();
+        /// CREA LA CLASE == INSTANCIAR LA CLASE CON VALOR
+        /// SortBubble y = new SortBubble();
+        SortBubble claseSortBubble = new SortBubble();
 
-        // Mostrar el arreglo original
-        System.out.print("Arreglo original: ");
-        System.out.println();
-        sortBubble.printArray(numeros);
+        // MOSTRAR EL ARREGLO ORIGINAL
+        System.out.println("Arreglo original:");
+        claseSortBubble.printArray(numeros); // IMPRIME
 
-    }
+        // ORDENAR ASCENDENTE
+        claseSortBubble.sortAscendente(numeros); // ORDENA
+        System.out.println("Ordenado ascendente:");
+        claseSortBubble.printArray(numeros); // IMPRIME
+
+        // ORDENAR DESCENDENTE
+        claseSortBubble.sortDescendente(numeros); // ORDENA
+        System.out.println("Ordenado descendente:");
+        claseSortBubble.printArray(numeros); // IMPRIME
+    }     
 }
--- src/SortBubble.java
+++ src/SortBubble.java
@@ -1,67 +1,53 @@
-public class SortBubble{
+public class SortBubble {
 
-    public SortBubble(){
-        
-    }
-    
-
-    public void sortAscendente(int[] numeros){
-        int contCambios = 0;
-        int contPreguntas = 0;
-
-        for (int i = 0; i < numeros.length - 1; i++) {
-            boolean hayCambio = false; 
-            for(int j = 0; j < numeros.length - 1 - i; j++){
-                contPreguntas ++;
-
-                if(numeros[j] >  numeros[j + 1]){ // no se cambia aqui
-                    int temp = numeros[j];
-                    numeros[j] = numeros[j + 1];
-                    numeros[j + 1] = temp;
-                    contCambios ++;
-                    hayCambio = true;
-                }
-            }
-            if(!hayCambio){
-                break;
-            }
-        }
+    public SortBubble() {
+        // Constructor vacío
     }
 
-
-    public void sortDescendente(int[] numeros){
-        int contCambios = 0;
-        int contPreguntas = 0;
-        
+    public void sortAscendente(int[] numeros) {
         for (int i = 0; i < numeros.length - 1; i++) {
             boolean hayCambio = false;
-            for(int j = 0; j < numeros.length - 1 - i; j++){
-                contPreguntas++;
-                
-                if(numeros[j] < numeros[j + 1]){ // Cambio aquí
+            for (int j = 0; j < numeros.length - 1 - i; j++) {
+                if (numeros[j] > numeros[j + 1]) {
                     int temp = numeros[j];
                     numeros[j] = numeros[j + 1];
                     numeros[j + 1] = temp;
-                    contCambios++;
                     hayCambio = true;
                 }
             }
-            if(!hayCambio){
+            if (!hayCambio) {
                 break;
             }
         }
     }
 
+    public void sortDescendente(int[] numeros) {
+        for (int i = 0; i < numeros.length - 1; i++) {
+            boolean hayCambio = false;
+            for (int j = 0; j < numeros.length - 1 - i; j++) {
+                if (numeros[j] < numeros[j + 1]) { // Cambio de signo
+                    int temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                    hayCambio = true;
+                }
+            }
+            if (!hayCambio) {
+                break;
+            }
+        }
+    }
 
+    // Método para imprimir el arreglo
     public void printArray(int[] numeros) {
         System.out.print("[");
         for (int i = 0; i < numeros.length; i++) {
             if (i == numeros.length - 1) {
                 System.out.print(numeros[i]);
-            }else
-                System.out.print(numeros[i] + " , ");
+            } else {
+                System.out.print(numeros[i] + ", ");
+            }
         }
         System.out.println("]");
     }
-
 }
